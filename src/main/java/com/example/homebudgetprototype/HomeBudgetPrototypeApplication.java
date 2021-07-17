8cd00fbac01cc34fda02910df7dca3eb05a883a3
Vyacheslav-Lapin <Vyacheslav.Lapin@gmail.com>
2021-07-17 14:15:22 +0300

    Initial commit

---
+++ src/main/java/com/example/homebudgetprototype/HomeBudgetPrototypeApplication.java
@@ -0,0 +1,12 @@
+package com.example.homebudgetprototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeBudgetPrototypeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HomeBudgetPrototypeApplication.class, args);
+  }
+}
